81f7cb35bab7610aeb65689690d59915431c58ae
Donizete Vida <donizetevida1998@outlook.com>
2022-08-13 15:58:12 -0300

    Background base style

--- app/src/main/java/com/ds/ds95/MainActivity.kt
+++ app/src/main/java/com/ds/ds95/MainActivity.kt
@@ -3,13 +3,17 @@ package com.ds.ds95
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.ds.ds95.ui.theme.Ds95Theme
 
 class MainActivity : ComponentActivity() {
@@ -29,9 +33,45 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Preview(
+    widthDp = 150,
+    heightDp = 80
+)
+@Composable
+fun Background(
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit = {}
+) {
+    Surface(
+        color = Color.Black,
+        modifier = modifier
+    ) {
+        Surface(
+            color = Color.White,
+            modifier = Modifier.padding(end = 2.dp, bottom = 2.dp)
+        ) {
+            Surface(
+                color = Color.DarkGray,
+                modifier = Modifier.padding(start = 2.dp, top = 2.dp)
+            ) {
+                Surface(
+                    color = Color.Gray,
+                    modifier = Modifier.padding(end = 2.dp, bottom = 2.dp)
+                ) {
+                    content()
+                }
+            }
+        }
+    }
+}
+
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Background(
+        modifier = Modifier.width(150.dp).height(80.dp)
+    ) {
+        Text(text = "Hello $name!")
+    }
 }
 
 @Preview(showBackground = true)
