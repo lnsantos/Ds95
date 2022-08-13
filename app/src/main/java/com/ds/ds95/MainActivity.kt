bc154dfd38c992a5f5b96a2dc970ec7edbb5565a
Donizete Vida <donizetevida1998@outlook.com>
2022-08-13 16:49:23 -0300

    Click animation

--- app/src/main/java/com/ds/ds95/MainActivity.kt
+++ app/src/main/java/com/ds/ds95/MainActivity.kt
@@ -3,15 +3,21 @@ package com.ds.ds95
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.focusable
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.focus.onFocusEvent
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.ds.ds95.ui.theme.Ds95Theme
@@ -19,6 +25,7 @@ import com.ds.ds95.ui.theme.Ds95Theme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             Ds95Theme {
                 // A surface container using the 'background' color from the theme
@@ -42,23 +49,46 @@ fun Background(
     modifier: Modifier = Modifier,
     content: @Composable () -> Unit = {}
 ) {
+    var pressed by remember { mutableStateOf(false) }
+
     Surface(
         color = Color.Black,
-        modifier = modifier
+        modifier = modifier.pointerInput(Unit) {
+            detectTapGestures(
+                onPress = {
+                    pressed = true
+                    awaitRelease()
+                    pressed = false
+                }
+            )
+        }
     ) {
         Surface(
-            color = Color.White,
-            modifier = Modifier.padding(end = 2.dp, bottom = 2.dp)
+            color = Color.Black,
+            modifier = Modifier
+                .background(color = Color.Black)
+                .padding(
+                    if (pressed) {
+                        2.dp
+                    } else {
+                        0.dp
+                    }
+                )
         ) {
             Surface(
-                color = Color.DarkGray,
-                modifier = Modifier.padding(start = 2.dp, top = 2.dp)
+                color = Color.White,
+                modifier = Modifier.padding(end = 2.dp, bottom = 2.dp)
             ) {
                 Surface(
-                    color = Color.Gray,
-                    modifier = Modifier.padding(end = 2.dp, bottom = 2.dp)
+                    color = Color.DarkGray,
+                    modifier = Modifier.padding(start = 2.dp, top = 2.dp)
                 ) {
-                    content()
+                    Surface(
+                        color = Color.Gray,
+                        modifier = Modifier.padding(end = 2.dp, bottom = 2.dp)
+                    ) {
+                        content()
+                    }
                 }
             }
         }
@@ -67,10 +97,22 @@ fun Background(
 
 @Composable
 fun Greeting(name: String) {
-    Background(
-        modifier = Modifier.width(150.dp).height(80.dp)
-    ) {
-        Text(text = "Hello $name!")
+    Column {
+        Background(
+            modifier = Modifier
+                .width(150.dp)
+                .height(80.dp)
+        ) {
+            Text(text = "Hello $name! 1")
+        }
+
+        Background(
+            modifier = Modifier
+                .width(150.dp)
+                .height(80.dp)
+        ) {
+            Text(text = "Hello $name! 2")
+        }
     }
 }
 
